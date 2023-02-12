990eafd803fcac63b81de1c1a7d22e0d333ccbb8
Adam Tuttle <adamtuttlecodes@gmail.com>
2023-02-12 18:08:14 -0500

    Select

---
+++ src/lib/inputs/Select.story.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	export let Hst: Hst;
+	import Select from '$lib/inputs/Select.svelte';
+</script>
+
+<Hst.Story title="inputs/Select" layout={{ type: 'grid', width: '300px' }}>
+	<Select value="rubber baby buggy bumpers">
+		<option value="bumpers">Bumpers</option>
+		<option value="rubber baby buggy bumpers">Rubber baby buggy bumpers</option>
+		<option value="baby buggy">Baby Buggy</option>
+		<option value="rubber baby">Rubber Baby</option>
+	</Select>
+</Hst.Story>
---
+++ src/lib/inputs/Select.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { getContext, hasContext } from 'svelte';
+
+	import { borderRadius, textInput } from '$lib/theme';
+
+	export let value: string = '';
+
+	let classPassThru: string = '';
+	export { classPassThru as class };
+
+	let classes = classPassThru + ` ${$borderRadius} ${$textInput}`;
+
+	const inInputGroup = hasContext('inputGroup') ? getContext('inputGroup') : false;
+	const inputGroupMode = hasContext('inputGroupMode') ? getContext('inputGroupMode') : null;
+	if (inInputGroup) {
+		classes = classes + ' flex-1';
+		if (inputGroupMode === 'left') {
+			classes = classes + ' rounded-l-none';
+		} else if (inputGroupMode === 'right') {
+			classes = classes + ' rounded-r-none';
+		} else if (inputGroupMode === 'both') {
+			classes = classes + ' rounded-none';
+		}
+	}
+</script>
+
+<select bind:value class={classes}>
+	<slot />
+</select>
