1b9a40ad7ef0547bb39aeafbbfd4e28257532106
Adam Tuttle <adamtuttlecodes@gmail.com>
2023-02-12 12:18:08 -0500

    TextArea

---
+++ src/lib/forms/TextArea.story.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	export let Hst: Hst;
+	import TextArea from '$lib/forms/TextArea.svelte';
+
+	let value: string =
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa at vitae praesentium est ratione magnam consequatur, enim, voluptatem sed animi doloremque molestias natus aspernatur nihil et officiis perferendis ipsa aliquid.';
+</script>
+
+<Hst.Story title="forms/TextArea" layout={{ type: 'grid', width: 500 }}>
+	<TextArea bind:value />
+</Hst.Story>
---
+++ src/lib/forms/TextArea.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { borderRadius, textInput } from '../theme';
+
+	export let value: string;
+	export let rows: number = 4;
+
+	let passThruClasses: string = '';
+	export { passThruClasses as class };
+	let classes = `${$borderRadius} ${$textInput} ${passThruClasses}`;
+</script>
+
+<textarea bind:value class={classes} {rows} />
