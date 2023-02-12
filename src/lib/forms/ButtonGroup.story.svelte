967957c81874383569a9cbec381aaa3b0fb8117c
Adam Tuttle <adamtuttlecodes@gmail.com>
2023-02-11 21:32:24 -0500

    ButtonGroup

---
+++ src/lib/forms/ButtonGroup.story.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	export let Hst: Hst;
+	import ButtonGroup from '$lib/forms/ButtonGroup.svelte';
+	import Button from '$lib/forms/Button.svelte';
+</script>
+
+<Hst.Story title="forms/ButtonGroup" layout={{ type: 'grid', width: 400 }}>
+	<Hst.Variant title="Horizontal">
+		<ButtonGroup>
+			<Button variant="cta">Foo</Button>
+			<Button variant="tertiary">Bar</Button>
+			<Button variant="brand">Baz</Button>
+		</ButtonGroup>
+	</Hst.Variant>
+	<Hst.Variant title="Vertical">
+		<ButtonGroup stack={true}>
+			<Button variant="cta">Foo</Button>
+			<Button variant="tertiary">Bar</Button>
+			<Button variant="brand">Baz</Button>
+		</ButtonGroup>
+	</Hst.Variant>
+</Hst.Story>
---
+++ src/lib/forms/ButtonGroup.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { setContext } from 'svelte';
+	import { borderRadius } from '$lib/theme';
+
+	setContext('buttonGroup', true);
+
+	export let stack: boolean = false;
+</script>
+
+<div class={`inline-flex ${stack ? 'flex-col' : ''} overflow-hidden ${$borderRadius}`}>
+	<slot />
+</div>
