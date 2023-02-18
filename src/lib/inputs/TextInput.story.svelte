25441405bd0ceb105f13faa6524c5f50906387fc
Adam Tuttle <adamtuttlecodes@gmail.com>
2023-02-17 22:19:25 -0500

    add debounce and loading for type=search, and some more fun field prefixes

--- src/lib/inputs/TextInput.story.svelte
+++ src/lib/inputs/TextInput.story.svelte
@@ -2,11 +2,26 @@
 	import type { Hst } from '@histoire/plugin-svelte';
 	export let Hst: Hst;
 	import TextInput from '$lib/inputs/TextInput.svelte';
+
+	let searchTerm: string = 'Real Housewives of Kronos';
+	let searchLoading: boolean = false;
+	function handleSearch() {
+		searchLoading = true;
+		setTimeout(() => {
+			searchLoading = false;
+		}, 5000);
+	}
 </script>
 
 <Hst.Story title="inputs/TextInput" layout={{ type: 'grid', width: '300px' }}>
 	<Hst.Variant title="Text">
 		<TextInput type="text" value="" />
+	</Hst.Variant>
+	<Hst.Variant title="Tel">
+		<TextInput type="tel" value="555-123-4567" />
+	</Hst.Variant>
+	<Hst.Variant title="URL">
+		<TextInput type="url" />
 	</Hst.Variant>
 	<Hst.Variant title="Email">
 		<TextInput type="email" />
@@ -14,19 +29,24 @@
 	<Hst.Variant title="Password">
 		<TextInput type="password" value="lol made you look" />
 	</Hst.Variant>
-	<Hst.Variant title="Tel">
-		<TextInput type="tel" value="555-123-4567" />
+	<Hst.Variant title="Search">
+		<TextInput
+			type="search"
+			bind:value={searchTerm}
+			debounce={500}
+			on:change={handleSearch}
+			bind:loading={searchLoading}
+		/>
+		<strong>debounced:</strong>
+		{searchTerm}
+		<br />
+		<strong>loading:</strong>
+		{String(searchLoading)}
 	</Hst.Variant>
 	<Hst.Variant title="Date">
 		<TextInput type="date" />
 	</Hst.Variant>
-	<Hst.Variant title="Search">
-		<TextInput type="search" value="real housewives of Kronos" />
-	</Hst.Variant>
 	<Hst.Variant title="Time">
 		<TextInput type="time" />
-	</Hst.Variant>
-	<Hst.Variant title="URL">
-		<TextInput type="url" />
 	</Hst.Variant>
 </Hst.Story>
--- src/lib/inputs/TextInput.svelte
+++ src/lib/inputs/TextInput.svelte
@@ -4,25 +4,41 @@
 	// https://svelte.dev/docs#template-syntax-attributes-and-props
 
 	import type { InputType } from '$lib/types';
-	import { getContext, hasContext } from 'svelte';
+	import { getContext, hasContext, createEventDispatcher } from 'svelte';
 	import type { HTMLInputAttributes } from 'svelte/elements';
 	interface $$Props extends HTMLInputAttributes {
 		type: InputType;
 		value?: string;
 		class?: string;
+
+		//defaults to 0 aka no debounce; provide #ms to debounce
+		debounce?: number;
+
+		loading?: boolean;
 	}
 
+	import InputGroup from '$lib/inputs/InputGroup.svelte';
+	import InputGroupText from '$lib/inputs/InputGroupText.svelte';
 	import { borderRadius, textInput } from '$lib/theme';
+
+	const dispatch = createEventDispatcher();
 
 	export let value: string = '';
 	export let type: InputType = 'text';
+
+	export let debounce: number = 0;
+	export let loading: boolean = false;
 
 	let passThruClasses: string = '';
 	export { passThruClasses as class };
 	let classes = `${$borderRadius} ${$textInput} ${passThruClasses}`;
 
-	const inInputGroup = hasContext('inputGroup') ? getContext('inputGroup') : false;
-	const inputGroupMode = hasContext('inputGroupMode') ? getContext('inputGroupMode') : null;
+	let inInputGroup = hasContext('inputGroup') ? getContext('inputGroup') : false;
+	let inputGroupMode = hasContext('inputGroupMode') ? getContext('inputGroupMode') : null;
+	if (type === 'search' || type === 'tel' || type === 'email' || type === 'url') {
+		inInputGroup = true;
+		inputGroupMode = 'left';
+	}
 	if (inInputGroup) {
 		classes = classes + ' flex-1';
 		if (inputGroupMode === 'left') {
@@ -35,9 +51,55 @@
 	}
 
 	//since we don't allow numeric inputs in this TextInput component, we can assume the value should be a string
+	let debounceTimer: ReturnType<typeof setTimeout>;
 	function handleInput(event: Event) {
-		value = (event.target as HTMLInputElement).value;
+		if (debounce === 0) {
+			value = (event.target as HTMLInputElement).value;
+			dispatch('change', value);
+			return;
+		}
+		clearTimeout(debounceTimer);
+		debounceTimer = setTimeout(() => {
+			value = (event.target as HTMLInputElement).value;
+			dispatch('change', value);
+		}, debounce);
 	}
 </script>
 
-<input {...$$restProps} {type} {value} class={classes} on:input={handleInput} />
+{#if type === 'search'}
+	<InputGroup>
+		<InputGroupText slot="left" position="left">
+			{#if loading}
+				<span class="animate-bounce">🔎</span>
+			{:else}
+				🔎
+			{/if}
+		</InputGroupText>
+		<input slot="middle" {...$$restProps} {type} {value} class={classes} on:input={handleInput} />
+	</InputGroup>
+{:else if type === 'tel'}
+	<InputGroup>
+		<InputGroupText slot="left" position="left">☎️</InputGroupText>
+		<input slot="middle" {...$$restProps} {type} {value} class={classes} on:input={handleInput} />
+	</InputGroup>
+{:else if type === 'email'}
+	<InputGroup>
+		<InputGroupText slot="left" position="left">✉️</InputGroupText>
+		<input slot="middle" {...$$restProps} {type} {value} class={classes} on:input={handleInput} />
+	</InputGroup>
+{:else if type === 'url'}
+	<InputGroup>
+		<InputGroupText slot="left" position="left">🔗</InputGroupText>
+		<input
+			slot="middle"
+			placeholder="https://..."
+			{...$$restProps}
+			{type}
+			{value}
+			class={classes}
+			on:input={handleInput}
+		/>
+	</InputGroup>
+{:else}
+	<input {...$$restProps} {type} {value} class={classes} on:input={handleInput} />
+{/if}
