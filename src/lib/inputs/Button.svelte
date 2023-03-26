f953374840c9e2441259e481cac080b84268e7c3
Adam Tuttle <adamtuttlecodes@gmail.com>
2023-03-26 13:16:18 -0400

    feat(Button): pass arbitrary props on to the <button>

--- src/lib/inputs/Button.svelte
+++ src/lib/inputs/Button.svelte
@@ -67,6 +67,6 @@
 	let allClasses = `${themeClasses} ${variantClasses} ${passThruClasses} inline-block no-underline font-semibold antialiased transition uppercase`;
 </script>
 
-<button on:click disabled={isDisabled} class={allClasses}>
+<button on:click disabled={isDisabled} class={allClasses} {...$$restProps}>
 	<slot />
 </button>
