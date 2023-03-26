343f9f8111afbece528d88c1930b33344b9045bd
Adam Tuttle <adamtuttlecodes@gmail.com>
2023-03-26 09:13:45 -0400

    feat(FormFieldDisplay): make wrapperClasses and labelClasses overridable, add overridable class attribute for value
    Also simplify label width context and require wrapping in a <Form>

--- src/lib/forms/FormFieldDisplay.svelte
+++ src/lib/forms/FormFieldDisplay.svelte
@@ -1,22 +1,25 @@
 <script lang="ts">
 	import { hasContext, getContext } from 'svelte';
+	import { textInput } from '$lib/theme';
+
+	if (!hasContext('form-label-width')) {
+		throw new Error('FormFieldDisplay must be used within a Form component');
+	}
+	const labelWidth: string = getContext('form-label-width');
 
 	export let label: string = '';
 	export let id: string = '';
 	export let stack: boolean = false;
 	export let value: string | number = '';
-
-	const labelWidth: string = hasContext('form-label-width')
-		? getContext('form-label-width')
-		: 'w-1/4';
-
-	const labelClasses = `inline-block h-full pt-2 pr-4 text-right font-semibold ${labelWidth}`;
-	const wrapperClasses = `flex max-w-full items-start py-2 ${stack ? 'flex-col' : ''}`;
+	export let labelClasses: string = `inline-block h-full pt-2 pr-4 text-right font-semibold ${labelWidth}`;
+	export let wrapperClasses: string = `flex max-w-full items-start py-2 ${stack ? 'flex-col' : ''}`;
+	let passThruClasses: string = `flex-1 border-neutral-500 bg-neutral-300 text-neutral-800 w-full ${$textInput}`;
+	export { passThruClasses as class };
 </script>
 
 <div class={wrapperClasses}>
 	<label for={id} class={labelClasses}>{label}</label>
-	<div class="flex-1">
+	<div {id} class={passThruClasses}>
 		<slot>{value}</slot>
 	</div>
 </div>
